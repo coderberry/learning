2b4a74d96b3ba64d0075da6ec73c550198a4fab7
Eric Berry <cavneb@gmail.com>
2011-09-07 11:37:14 -0600

    added design patterns folder

---
+++ design_patterns/composite/make_batter_task.rb
@@ -0,0 +1,8 @@
+class MakeBatterTask < CompositeTask
+  def initialize
+    super('Make batter')
+    add_sub_task( AddDryIngredientsTask.new )
+    add_sub_task( AddLiquidsTask.new )
+    add_sub_task( MixTask.new )
+  end
+end
